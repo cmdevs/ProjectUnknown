6b2f67c905877a559623c3bcd1848ea577c3cb54
lcyer <ksah123@naver.com>
2018-05-15 01:29:29 +0900

    - branch test

--- app/src/main/java/com/cmdevs/projectunknown/MainActivity.kt
+++ app/src/main/java/com/cmdevs/projectunknown/MainActivity.kt
@@ -5,8 +5,13 @@ import android.os.Bundle
 
 class MainActivity : AppCompatActivity() {
 
+    val helloworld : String = "helloKotlin"
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        println(helloworld)
+
     }
 }
